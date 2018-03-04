a6a66acd965603ab63da354c4789249777e99b8e
abhay0019 <abhaysinghbishen@gmail.com>
2018-03-04 14:19:08 +0530

    first commit

---
+++ start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import pyttsx
+import subprocess as s
+import pyttsx
+import os
+#os.environ['DISPLAY']=":99"
+#os.system("vlc --play-and-exit --intf dummy /home/abhay/Desktop/py/terminal-startup/acdc.mov 2> /home/abhay/Desktop/py/terminal-startup/1.txt")
+#os.environ['DISPLAY']=":0.0"
+s.call(["notify-send","-i", "/home/abhay/Downloads/photo/old_mobile/Photo0068.jpg","Bhai ki Jai ho _/\_ :P A reverse shell !!"])
+#engine=pyttsx.init()
+#engine.say("Bhai")
+#engine.say("kee")
+#engine.say("jai")
+#engine.say("ho")
+#engine.runAndWait()
